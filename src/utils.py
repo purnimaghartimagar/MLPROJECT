af641cd783f01c0610b402044e6c9e0c61578d9e
purnima magar <purnimagmagar@gmail.com>
2026-01-22 14:26:34 +0545

    model_trained

--- src/utils.py
+++ src/utils.py
@@ -20,7 +20,7 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-    def evaluate_models(x_train,y_train,x_test,y_test,models,param):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         try:
             report={}
             for i in range(len(list(models))):
@@ -28,19 +28,19 @@ def save_object(file_path,obj):
                 para=param[list(models.keys())[i]]
                 #hyperparameter tuning
                 gs=GridSearchCV(model,para,cv=3)
-                gs.fit(x_train,y_train)
+                gs.fit(X_train,y_train)
                 #model training using best params
                 model.set_params(**gs.best_params_)
-                model.fit(x_train,y_train)
+                model.fit(X_train,y_train)
                 #prediction
-                y_train_pred=model.predict(x_train)
-                y_test_pred=model.predict(x_test)
+                y_train_pred=model.predict(X_train)
+                y_test_pred=model.predict(X_test)
                 
                 #r2 score
                 train_model_score=r2_score(y_train,y_train_pred)
                 test_model_score=r2_score(y_test,y_test_pred)
                 
-                report[list(model.keys())[i]]=test_model_score
+                report[list(models.keys())[i]]=test_model_score
                 return report
             
                 
